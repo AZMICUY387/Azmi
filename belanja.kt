e4ef8cfd58aa840b950c9a608e9399defc37da42
PAYAD <55588164+AZMICUY387@users.noreply.github.com>
2024-01-27 11:10:09 +0700

    Update belanja.kt

--- belanja.kt
+++ belanja.kt
@@ -10,25 +10,41 @@ fun barang(item : String):Int{
         else -> 0
     } 
 }
-
-fun diskon(harga: Int, diskon: Double): Int{
-    return (harga * diskon).toInt()
-    
+//fungsi dengan return
+fun barang(item : String):Int{
+    return when (item){
+    	"Gamis" -> 150000
+        "Kaos" -> 50000
+        "Kemeja" -> 700000
+        "Celana" -> 120000
+        "Kaos Dalam" -> 20000
+        
+        else -> 0
+    } 
 }
-fun total(harga: Int, hargaDiskon: Int): Int{
-    return (harga - hargaDiskon).toInt()
+
+fun diskon(harga: Int):Double{
     
+    if(harga >= 100000){
+        val diskon = harga * 0.01
+    	val total = harga - diskon
+        return total
+	}
+    else if(harga >= 80000){
+        val diskon = harga * 0.10
+    	val total = harga - diskon
+        return total
+	}
+    else {
+    return harga
+    }
+
 }
 
 fun main() {
-    val namaBarang = "Gamis"
-    val diskon = 0.05
-    val harga = barang(namaBarang)
-    val hargaDiskon= if (harga>100000) diskon(harga,diskon) else 0
-    val hargaTotal = total(harga, hargaDiskon)
-    println("Baju yang anda beli: \nNama barang\t: $namaBarang \nHarga Barang\t: Rp. $harga")
-    println("Diskon \t\t: Rp. $hargaDiskon")
-    println("Total Bayar \t: Rp. $hargaTotal")
+    val namabrgg = "Celana"
+    val harga = barang(namabrgg)
     
+    val total = diskon(harga)
+    println("Terima Kasih Telah Berbelanja Di BAyu SHOPPP\nNama Barang\t:\t$namabrgg\nTOTAL \t\t:\t $total")
 }
-
